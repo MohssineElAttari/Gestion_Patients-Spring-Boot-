35bf90d5f9cbab6fb3b4db41a722d67e2d9f8754
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-01-27 16:09:27 +0100

    first commit

---
+++ src/main/java/ma/youcode/demo22/entities/Patien.java
@@ -0,0 +1,23 @@
+package ma.youcode.demo22.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+@Entity
+@Table(name = "patiens")
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Patien {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @Column(name = "date_naissance")
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+    private int score;
+    private boolean malade;
+}
