6663a8b9ec7a74e17edfffd93ae7bb515d8c6c13
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-01-29 13:20:20 +0100

    add patient controller

--- src/main/java/ma/youcode/demo22/Controller/PatientController.java
+++ src/main/java/ma/youcode/demo22/Controller/PatientController.java
@@ -1,2 +1,26 @@
-package ma.youcode.demo22.Controller;public class PatientController {
+package ma.youcode.demo22.Controller;
+
+import ma.youcode.demo22.entities.Patien;
+import ma.youcode.demo22.repositories.PatientRipository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PatientController {
+    @Autowired
+    private PatientRipository patientRipository;
+
+    @GetMapping("/patients")
+    public List<Patien> patiens(){
+        return patientRipository.findAll();
+    }
+
+    @GetMapping("/patient/{id}")
+    public Patien getPatien(@PathVariable Long id){
+        return patientRipository.findById(id).get();
+    }
 }
