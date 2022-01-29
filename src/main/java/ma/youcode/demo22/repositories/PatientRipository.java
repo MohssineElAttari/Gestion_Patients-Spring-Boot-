1f511ee19e2b86043cf7bebd65f2d565646b7d47
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-01-29 13:25:02 +0100

    update in patient ripository

--- src/main/java/ma/youcode/demo22/repositories/PatientRipository.java
+++ src/main/java/ma/youcode/demo22/repositories/PatientRipository.java
@@ -1,7 +1,14 @@
 package ma.youcode.demo22.repositories;
 
 import ma.youcode.demo22.entities.Patien;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRipository extends JpaRepository<Patien,Long> {
+//    public List<Patien> findByNomContains(String name);
+    public Page<Patien> findByNomContains(String name, Pageable pageable);
+    public List<Patien> findByMalade(boolean malade);
 }
